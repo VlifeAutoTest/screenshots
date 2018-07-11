7986a744e8deb894d70c6b1c97599708453237b4
gaoyaxuan <2657279980@qq.com>
2018-07-11 15:28:16 +0800

    登录登出初次完成!项目备份!

---
+++ SpringHibernateExample/src/main/java/com/vlife/springmvc/service/UserService.java
@@ -0,0 +1,11 @@
+package com.vlife.springmvc.service;
+
+import java.util.List;
+
+import com.vlife.springmvc.model.User;
+
+public interface UserService {
+	void saveUser(User user);
+	Boolean findByName(String name);
+	List<User>  findUserByName(String name);
+}
